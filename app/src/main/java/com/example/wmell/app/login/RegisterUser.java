f62d14e7b245744e4ef8041a8b915543c34c6bbd
WIllian Melo <w.melo.fernandes.un@gmail.com>
2018-10-20 03:22:49 -0300

    Create Register Layout and basic logic

--- app/src/main/java/com/example/wmell/app/login/RegisterUser.java
+++ app/src/main/java/com/example/wmell/app/login/RegisterUser.java
@@ -1,43 +1,48 @@
 package com.example.wmell.app.login;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.wmell.app.R;
+import com.example.wmell.app.main.MainScreen;
+import com.example.wmell.app.util.Utils;
 
 public class RegisterUser extends AppCompatActivity {
 
-    private EditText mUser;
+    private EditText mUsername;
     private EditText mPassword;
+    private EditText mEmail;
+    private Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_user);
 
-        //mUser = findViewById(R.id.editText_user_register);
-        //mPassword = findViewById(R.id.editText_password_register);
-        //Button registerButton = findViewById(R.id.button_register);
+        mUsername = findViewById(R.id.editTextName);
+        mPassword = findViewById(R.id.editTextPassword);
+        mEmail = findViewById(R.id.editTextEmail);
+        mRegisterButton = findViewById(R.id.register_button);
 
-//        registerButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (Utils.isFilled(mUser.getText().toString(), mPassword.getText().toString())) {
-//                    Toast.makeText(RegisterUser.this, "User registered!", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(RegisterUser.this, R.style.Theme_AppCompat));
-//                    builder.setMessage(getString(R.string.fill_empty_fields));
-//                    builder.setPositiveButton(getString(R.string.ok_dialog_alert_text), new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface dialogInterface, int i) {
-//
-//                        }
-//                    });
-//                    builder.show();
-//                }
-//            }
-//        });
+
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Utils.isFilled(mUsername.getText().toString()) && Utils.isFilled(mEmail.getText().toString()) &&
+                        Utils.isFilled(mPassword.getText().toString())) {
+                    startActivity(new Intent(RegisterUser.this, MainScreen.class));
+                } else {
+                    Toast.makeText(RegisterUser.this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
