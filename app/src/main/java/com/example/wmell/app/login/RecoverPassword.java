563d5cb2c33f896dc9a5d47f10741b737646d287
WIllian Melo <w.melo.fernandes.un@gmail.com>
2018-10-20 03:58:30 -0300

    Change toast for a dialog(Register and Recover Activities)

--- app/src/main/java/com/example/wmell/app/login/RecoverPassword.java
+++ app/src/main/java/com/example/wmell/app/login/RecoverPassword.java
@@ -1,8 +1,11 @@
 package com.example.wmell.app.login;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -48,7 +51,15 @@ public class RecoverPassword extends AppCompatActivity {
                     buttonNewPassword.setVisibility(View.VISIBLE);
                     buttonRecover.setVisibility(View.GONE);
                 } else {
-                    Toast.makeText(RecoverPassword.this, "Try again!", Toast.LENGTH_SHORT).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(RecoverPassword.this, R.style.Theme_AppCompat));
+                    builder.setMessage(getString(R.string.try_again_alert));
+                    builder.setPositiveButton(getString(R.string.ok_dialog_alert_text), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    builder.show();
                 }
             }
         });
--- app/src/main/java/com/example/wmell/app/login/RegisterUser.java
+++ app/src/main/java/com/example/wmell/app/login/RegisterUser.java
@@ -1,9 +1,12 @@
 package com.example.wmell.app.login;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,7 +42,15 @@ public class RegisterUser extends AppCompatActivity {
                         Utils.isFilled(mPassword.getText().toString())) {
                     startActivity(new Intent(RegisterUser.this, MainScreen.class));
                 } else {
-                    Toast.makeText(RegisterUser.this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(RegisterUser.this, R.style.Theme_AppCompat));
+                    builder.setMessage(getString(R.string.fill_empty_fields));
+                    builder.setPositiveButton(getString(R.string.ok_dialog_alert_text), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    builder.show();
                 }
 
             }
